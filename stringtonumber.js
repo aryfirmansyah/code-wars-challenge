a2145326b3328b65b4ab702578ec5bb7727a8254
aryfirmansyah <aryfirmansyah28@gmail.com>
2024-02-07 17:58:00 +0700

    7 february 2024 2/90

---
+++ stringtonumber.js
@@ -0,0 +1,15 @@
+// const stringToNumber = function (str) {
+// //    let result = 0
+// //    if (result == String) {
+// //       typeof(result);
+// //    }
+
+//    return function(str);
+// };
+
+function stringToNumber(str) {
+   let result = str;
+   return result * 1;
+}
+
+console.log(stringToNumber("1234"));
