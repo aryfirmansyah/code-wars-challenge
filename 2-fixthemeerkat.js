43b0d52462d88aa258468362bb0d55cb4e778f0d
aryfirmansyah <aryfirmansyah28@gmail.com>
2024-02-08 22:52:53 +0700

    8 January 2024 5/90

---
+++ 2-fixthemeerkat.js
@@ -0,0 +1,8 @@
+//code wars 4/90
+//https://www.codewars.com/kata/56f699cd9400f5b7d8000b55
+
+function fixTheMeerkat(arr) {
+   return arr.reverse();
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"]));
